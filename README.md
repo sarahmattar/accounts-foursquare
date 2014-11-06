9c4c6fd968595a27ee839cc3100cdbe37f316968
Michael Samoylov <michael.samoylov@gmail.com>
2014-11-06 20:33:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# accounts-foursquare
+
+A login service for Foursquare. See the [project page](https://www.meteor.com/accounts) on Meteor Accounts for more details.
---
+++ package.js
@@ -0,0 +1,16 @@
+ackage.describe({
+    summary: "Login service for Foursquare accounts",
+    version: "0.1"
+});
+
+Package.on_use(function(api) {
+    api.use('accounts-base', ['client', 'server']);
+    // Export Accounts (etc) to packages using this one.
+    api.imply('accounts-base', ['client', 'server']);
+    api.use('accounts-oauth', ['client', 'server']);
+    api.use('foursquare', ['client', 'server']);
+
+    api.add_files('foursquare_login_button.css', 'client');
+
+    api.add_files("foursquare.js");
+});

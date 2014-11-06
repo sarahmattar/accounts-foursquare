154e4d45dcc3c374911430908340c6bb43c17a0a
Michael Samoylov <michael.samoylov@gmail.com>
2014-11-06 22:41:31 +0200

    version bump, misc. fixes

--- package.js
+++ package.js
@@ -1,15 +1,17 @@
 Package.describe({
+    name: 'msamoylov:accounts-foursquare',
     summary: "Login service for Foursquare accounts",
-    version: "1.0.0",
+    version: "1.0.1",
     git: "https://github.com/msamoylov/accounts-foursquare.git"
 });
 
 Package.on_use(function(api) {
+    api.versionsFrom('1.0');
     api.use('accounts-base', ['client', 'server']);
     // Export Accounts (etc) to packages using this one.
     api.imply('accounts-base', ['client', 'server']);
     api.use('accounts-oauth', ['client', 'server']);
-    api.use('foursquare', ['client', 'server']);
+    api.use('msamoylov:foursquare@1.0.0', ['client', 'server']);
 
     api.add_files('foursquare_login_button.css', 'client');
 

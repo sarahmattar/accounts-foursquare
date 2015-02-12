937fc3a53daa53bf36f7ba71ccea3f9a3afe7a05
Rick Wehrle <rick@wehrle.net>
2015-02-11 22:51:51 -0500

    update package version

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-    name: 'msamoylov:accounts-foursquare',
+    name: 'wehrlock:accounts-foursquare',
     summary: "Login service for Foursquare accounts",
-    version: "1.0.3",
-    git: "https://github.com/msamoylov/accounts-foursquare.git"
+    version: "1.0.4",
+    git: "https://github.com/wehrlock/accounts-foursquare.git"
 });
 
 Package.on_use(function(api) {
@@ -11,7 +11,7 @@ Package.on_use(function(api) {
     // Export Accounts (etc) to packages using this one.
     api.imply('accounts-base', ['client', 'server']);
     api.use('accounts-oauth', ['client', 'server']);
-    api.use('msamoylov:foursquare@1.0.3', ['client', 'server']);
+    api.use('wehrlock:foursquare@1.0.4', ['client', 'server']);
 
     api.add_files('foursquare_login_button.css', 'client');
 
